cf751386325c8de7f91aa03ef62d6f225c6ca5c7
Cowichan <fourniercowichan@gmail.com>
2019-06-29 16:43:26 +0200

    leaflet 2

---
+++ js/map.js
@@ -0,0 +1,14 @@
+var ga = [44.558611, 6.076561];
+
+//creation de la map
+var mymap = L.map('map').setView(ga, 15);
+
+// creation du calque images
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 48
+}).addTo(mymap);
+
+var marker = L.marker(ga).addTo(mymap);
+
+// ajout d'un popup
+marker.bindPopup('<h3>Monsieur Café Plumer</h3>');
